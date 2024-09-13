915cacb253dc068baa11dc487414e3cba55ba689
Brandon Buckley <brandonmb0136@gmail.com>
2024-09-13 15:58:28 -0500

    Add custom locking

--- wm/i3.nix
+++ wm/i3.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }: {
+{ pkgs, lib, ... }: {
   config = {
     services.xserver = {
       enable = true;
@@ -9,20 +9,28 @@
     };
     services.displayManager = { defaultSession = "none+i3"; };
     services.picom.enable = true;
-    home-manager.sharedModules = [{
-      xsession.windowManager.i3 = {
-        config = let modifier = "Mod4";
-        in {
-          inherit modifier;
-          gaps.inner = 8;
-          gaps.outer = 12;
-          defaultWorkspace = "workspace number 1";
-          keybindings = lib.mkOptionDefault {
-            "${modifier}+Return" = "exec alacritty";
-            "${modifier}+Shift+Return" = "exec alacritty -e tmux";
+    home-manager.sharedModules = [
+      (let
+        locker = "${pkgs.i3lock-fancy}/bin/i3lock";
+      in {
+        services.screen-locker = {
+          enable = true;
+          lockCmd = locker;
+        };
+        xsession.windowManager.i3 = {
+          config = let modifier = "Mod4";
+          in {
+            inherit modifier;
+            gaps.inner = 8;
+            gaps.outer = 12;
+            defaultWorkspace = "workspace number 1";
+            keybindings = lib.mkOptionDefault {
+              "${modifier}+Return" = "exec alacritty";
+              "${modifier}+Shift+Return" = "exec alacritty -e tmux";
+              "${modifier}+L" = "exec ${locker}";
+            };
           };
         };
-      };
-    }];
+      })];
   };
 }
