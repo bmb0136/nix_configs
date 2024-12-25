c4e83d52a163a242c7e9e4a2de149563284fa929
Brandon Buckley <brandonmb0136@gmail.com>
2024-12-25 15:38:57 -0600

    Temp fix

--- systems/manta/configuration.nix
+++ systems/manta/configuration.nix
@@ -67,10 +67,10 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+  # networking.firewall.allowedTCPPorts = [ 22 80 443 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
