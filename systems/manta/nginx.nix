936479ad83474940ca1ac1f9fe81d33d215aa97f
Brandon Buckley <brandonmb0136@gmail.com>
2025-02-08 23:19:53 +0000

    Fix 404 status config

--- systems/manta/nginx.nix
+++ systems/manta/nginx.nix
@@ -4,7 +4,10 @@
     recommendedProxySettings = true;
     virtualHosts."manta.zt" = {
       default = true;
-      locations."/".return = ''404 "<p>Not Found :(</p>"'';
+      locations."/" = {
+        return = ''404 "<p>Not Found :(</p>"'';
+        extraConfig = ''add_header Content-Type text/html;'';
+      };
     };
   };
 }
