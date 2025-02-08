f46b290b63b2676506a6d87d885f0ac8bb67544e
Brandon Buckley <brandonmb0136@gmail.com>
2025-02-08 23:01:38 +0000

    Adding nginx; update gitea config to use proxy

--- systems/manta/gitea.nix
+++ systems/manta/gitea.nix
@@ -1,5 +1,21 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   services.gitea = {
     enable = true;
+    settings = lib.mkIf config.services.nginx.enable {
+      server.ROOT_URL = "http://git.manta.zt/";
+    };
+  };
+  services.nginx.virtualHosts = lib.mkIf config.services.nginx.enable {
+    virtualHosts."git.manta.zt" = {
+      locations."/" = {
+        proxyWebsockets = true;
+        proxyPass = "http://127.0.0.1:3000";
+        extraConfig = "client_max_body_size 512M;";
+      };
+    };
   };
 }
