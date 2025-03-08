b4df4ddde284b20eb138229b8a3e6febede13c67
Brandon Buckley <brandonmb0136@gmail.com>
2025-03-08 03:56:56 +0000

    Update wallpaper

--- themes/catppuccin.nix
+++ themes/catppuccin.nix
@@ -3,32 +3,29 @@
   pkgs,
   lib,
   ...
-}:
-{
-  imports = [ inputs.stylix.nixosModules.stylix ];
-  config =
-    let
-      opacity = 0.75;
-    in
-    {
-      stylix = {
-        enable = true;
-        image = ./nixos.png;
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-        fonts = {
-          monospace = {
-            # package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
-            package = pkgs.nerd-fonts.jetbrains-mono;
-            name = "JetBrainsMono Nerd Font";
-          };
+}: {
+  imports = [inputs.stylix.nixosModules.stylix];
+  config = let
+    opacity = 0.75;
+  in {
+    stylix = {
+      enable = true;
+      image = ./keep_coding.png;
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+      fonts = {
+        monospace = {
+          # package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+          package = pkgs.nerd-fonts.jetbrains-mono;
+          name = "JetBrainsMono Nerd Font";
         };
-        opacity.applications = opacity;
       };
-      home-manager.sharedModules = [
-        {
-          programs.alacritty.settings.window.opacity = lib.mkForce opacity;
-          programs.alacritty.settings.font.size = lib.mkForce 8;
-        }
-      ];
+      opacity.applications = opacity;
     };
+    home-manager.sharedModules = [
+      {
+        programs.alacritty.settings.window.opacity = lib.mkForce opacity;
+        programs.alacritty.settings.font.size = lib.mkForce 8;
+      }
+    ];
+  };
 }
